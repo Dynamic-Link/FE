64378435ca9514133a3a550dfa1329fd9e6de145
john2796 <jbmiranda22796@gmail.com>
2020-01-24 12:47:01 -0800

    Got christian code

--- frontend/src/components/Links/LinkItem.js
+++ frontend/src/components/Links/LinkItem.js
@@ -2,16 +2,19 @@ import React from "react"
 
 export const LinkItem = ({ classes, link }) => {
   return (
-    <div className={classes.linkBox}>
-      <h2 className={classes.linkName}>{link.linkName}</h2>
+    <div className={classes.linkBoxContainer}>
+      <span></span>
+      <div className={classes.linkBox}>
+        <h2 className={classes.linkName}>{link.linkName}</h2>
 
-      <p>Default URL: {link.defaultUrl}</p>
-      <p>Promotions: {link.promotions}</p>
-      <div className={classes.clicksAndUsers}>
-        <p className={classes.notes}>Notes: {link.notes}</p>
-        <p>Total Clicks</p>
-        <p></p>
-        <p>Unique Users</p>
+        <p>Default URL: {link.defaultUrl}</p>
+        <p>Promotions: {link.promotions}</p>
+        <div className={classes.clicksAndUsers}>
+          <p className={classes.notes}>Notes: {link.notes}</p>
+          <p>Total Clicks</p>
+          <p></p>
+          <p>Unique Users</p>
+        </div>
       </div>
     </div>
   )
